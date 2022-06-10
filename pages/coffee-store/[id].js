b60ef6776a61e91b3d57ed4044f06e5ff5b3141d
almar-a <almar-a@auto.kz>
2022-06-10 14:59:30 +0600

    Feature: Add dynamic route and styles

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const CoffeeStore = () => {
+	return (
+		<div>
+			Coffee Store Page
+			<Link href='/'><a>Back to home</a></Link>
+		</div>
+	)
+}
+
+export default CoffeeStore
